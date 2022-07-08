a8c2181b17f0f4f1adc8a2302a3c77a42c52e7ab
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-08 20:17:18 +0400

    Update main.js

--- task-7/main.js
+++ task-7/main.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let str = "kpkkp";
+let arr = str.split("p");
+let a=-Infinity;
+for (i=0; i<arr.length; i++) {
+  if(arr[i].length>a ){
+    a=arr[i].length
+  }
+}
+console.log(a);
