1b59fbf4367a16b7dcc0a4a8b6dfb613f4f68cc0
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-08 19:28:02 +0400

    Update main.js

--- task-1/main.js
+++ task-1/main.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let str = "31,15,40";
+let min= Infinity;
+let arr = str.split(",");
+for(i=0; i<arr.length; i++){
+if(arr[i]<min){
+min=arr[i]
+}
+}
+console.log(min)
