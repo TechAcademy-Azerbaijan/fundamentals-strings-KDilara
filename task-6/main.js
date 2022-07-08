73a097f36c354dad6b489a31558b7697bee5d533
KDilara <106311483+KDilara@users.noreply.github.com>
2022-07-08 20:04:32 +0400

    Update main.js

--- task-6/main.js
+++ task-6/main.js
@@ -1,9 +1,9 @@
-const prompt = require('prompt');
-prompt.start();
-
-
-prompt.get('input', function (err, result) {
-  
-    // Write code here
-  
-});
+let str = "Hello world! Hello,    country!";
+let arr = str.split(" ");
+let count = 0;
+for (i=0; i<arr.length; i++) {
+  if (arr[i] != "") {
+    count++
+  }
+}
+console.log(count);
